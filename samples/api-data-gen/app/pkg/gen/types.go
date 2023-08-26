36ed02dbad35749c74521c7c987903ed4287b084
Kunal L <kunallimaye@gmail.com>
2023-08-26 12:02:59 +1000

    Refactoring

--- samples/api-data-gen/app/pkg/gen/types.go
+++ samples/api-data-gen/app/pkg/gen/types.go
@@ -1,7 +1,6 @@
 package gen
 
 import (
-	"github.com/finiteloopme/demos/samples/api-data-gen/app/pkg/api"
 	"github.com/finiteloopme/demos/samples/api-data-gen/app/pkg/types"
 )
 
@@ -10,12 +9,4 @@ type ApiGen struct {
 	Api   types.ApiType `json:"spec"`
 	Uri   types.UriType `json:"uri"`
 	Paths []*types.Path `json:"paths"`
-}
-
-func (apiGen *ApiGen) LoadApiSpec(apiSpec *api.ApiSpec) error {
-	apiGen.Api = apiSpec.Api
-	apiGen.Uri = apiSpec.Uri
-	apiGen.Paths = apiSpec.Paths
-
-	return nil
 }
